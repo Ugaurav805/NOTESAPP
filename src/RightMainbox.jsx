4ed11bcfb36eef8edfd8cee4acedc722960e6957
Ugaurav805 <ugaurav805@gmail.com>
2025-01-19 23:20:24 +0530

    .

--- src/RightMainbox.jsx
+++ src/RightMainbox.jsx
@@ -36,7 +36,7 @@ const RightMainbox = ({ activeGroup, onSendMessage }) => {
       <div className="RightMainbox">
         <img
           className="image"
-          src="src/assets/Notes.png"
+          src="https://s3-alpha-sig.figma.com/img/f2b5/d356/00b6d4748cd536df01bd2b4fecc1d821?Expires=1737936000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=NfQR3vqqO9Hwzxtx-MkI44tAhDebMQVPitweha1WjhgTLw764IXurUsVqjR2Ql3b6VSNghWTTgaPWgeG-M1-5v88wekaD32W9DCgEUVtpfTzeppQaznJEvCcD~4wtLIokhn78EI5~uCZ1~FwMBf-aDLN0iqqJIEjxr67HJKZWwsaq~LrJTWqOA0b9wW2doyn1GSS4r1PVQjRCtIyfgIxJ-mttE3gQFC07G6YxuDshDopRLlhytwZ-NxKrlz2whNQ~lAywYBO3w6y6Yk8GVKQCVASndqxARBbOFnabJck81tx~WNzuuoCYVVdNecZtHx49Nn9eQD82JPvyVgemYvvWQ__"
           alt="Pocket Notes Illustration"
           style={{
             width: "526px",
